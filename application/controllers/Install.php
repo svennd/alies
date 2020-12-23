1780754f0e6a658415189de29310329cce5badec
svenn <svenn.dhert@uantwerpen.be>
2020-12-23 23:36:34 +0100

    initial work on installer
    Signed-off-by: svenn <svenn.dhert@uantwerpen.be>

---
+++ application/controllers/Install.php
@@ -0,0 +1,40 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Install extends Frontend_Controller {
+
+	# constructor
+	public function __construct()
+	{
+		parent::__construct();
+		
+		# load librarys
+		$this->load->helper('url');	
+		$this->load->helper('file');
+	}
+	
+	public function start()
+	{
+		$data = array(
+						"writable" => (bool) (is_writable('application/config/config.php') && is_writable('application/config/database.php')),
+					);
+		$this->_render_page("first", $data);
+	}
+	public function second()
+	{
+		$this->_render_page("second", array());
+	}
+	public function third()
+	{
+		var_dump($this->input->post());
+		
+		$this->_render_page("third", array());
+	}
+	
+	private function _render_page($page, $data)
+	{
+		$this->load->view('install/header', $data);
+		$this->load->view('install/'. $page, $data);
+		$this->load->view('install/footer', $data);
+	}
+}
