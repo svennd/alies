f9e9edd387e820df31026c24a35f60b5739e0696
svenn <svenn.dhert@uantwerpen.be>
2020-12-29 20:52:36 +0100

    - #1 add support for auto calculations - change position of payment
    Signed-off-by: svenn <svenn.dhert@uantwerpen.be>

--- application/views/bill_report.php
+++ application/views/bill_report.php
@@ -27,7 +27,63 @@ $state = array(
 			<a href="<?php echo base_url(); ?>invoice">Invoice</a> / <a href="<?php echo base_url(); ?>owners/detail/<?php echo $owner['id']; ?>"><?php echo $owner['last_name'] ?></a> 
 		</div>
 		<div class="card-body">
-			<div class="card-body p-0">				
+			<div class="card-body p-0">		
+	<?php if ($bill['status'] != PAYMENT_PAID): ?>
+					<p class="lead">Payment : <?php echo $state[$bill['status']]; ?></p>
+					<?php 
+						$cash = round((float) $bill['cash'], 2);
+						$card = round((float) $bill['card'], 2);
+						if ($card + $cash != 0) :
+							$total_short = round( (float) $bill['amount'], 2) - ($card + $cash);
+							?>
+								<p>
+									Short : <?php echo $total_short; ?> &euro; (card : <?php echo $card; ?> &euro;, cash : <?php echo $cash; ?> &euro;)
+								</p>
+						<?php endif; ?>
+						
+					<form action="<?php echo base_url(); ?>invoice/bill_pay/<?php echo $bill_id ?>" method="post" autocomplete="off">
+		<div class="input-group mb-3">
+			<div class="input-group-prepend">
+				<span class="input-group-text" for="exampleCheck1"><a href="#" id="select_card" onclick="event.preventDefault()"><i class="fab fa-cc-visa"></i>&nbsp;Card</a></span>
+			</div>
+			<input type="text" class="form-control" id="card_value" name="card_value" value="<?php echo ($card != 0) ? $card:'';?>" />
+			<div class="input-group-append">
+				<span class="input-group-text">&euro;</span>
+			</div>
+		</div>
+		
+		<div class="input-group mb-3">
+		  <div class="input-group-prepend">
+			<span class="input-group-text" for="exampleCheck1"><a href="#" id="select_cash" onclick="event.preventDefault()"><i class="fas fa-euro-sign"></i>&nbsp; Cash</a></span>
+		  </div>
+		  <input type="text" class="form-control" id="cash_value" name="cash_value" value="<?php echo ($cash != 0) ? $cash:'';?>" />
+		<div class="input-group-append">
+			<span class="input-group-text">&euro;</span>
+		</div>
+		<div class="input-group-append">	
+			<span class="input-group-text" id="calculate"><a href="#"><i class="fas fa-calculator"></i></a></span>
+		</div>
+		</div>
+		<i><small id="payment_info" class="form-text text-muted ml-2">&nbsp;</small></i>
+			<button type="submit" name="submit" value="1" class="btn btn-outline-success my-3 mx-3"><i class="fas fa-file-invoice-dollar"></i> Payment Complete</button>
+		
+		<?php endif; ?>
+		<?php if ($bill['status'] == PAYMENT_PAID): ?>
+		<p class="lead">Payment : <?php echo $state[$bill['status']]; ?>!</p>
+		<?php 
+			$cash = round((float) $bill['cash'], 2);
+			$card = round((float) $bill['card'], 2);
+		?>
+		Payed : <?php echo $bill['amount']; ?> &euro; (card : <?php echo $card; ?> &euro;, cash : <?php echo $cash; ?> &euro;)
+		<!-- <a href="<?php echo base_url(); ?>invoice/print_bill/<?php echo $bill_id; ?>" class="btn btn-outline-secondary my-3 mx-3" target="_blank"><i class="fas fa-print"></i> print</a> -->
+
+		<?php endif; ?>
+			<?php if ($bill['status'] != PAYMENT_PAID): ?>
+			<a href="<?php echo base_url(); ?>invoice/bill_unpay/<?php echo $bill_id; ?>" class="btn btn-outline-danger my-3 mx-3" target="_blank"><i class="fas fa-syringe"></i> Drop from stock</a>	
+		<?php endif; ?>
+		  </form>		
+
+					<hr class="mt-5">		  
 			<h4>Bill #<?php
 				$date = date_create_from_format ('Y-m-d H:i:s', $bill['created_at']); 
 					echo date_format($date, 'Y') . str_pad($bill['id'], 5, '0', STR_PAD_LEFT); ?></h4>
@@ -125,58 +181,6 @@ $state = array(
                         </div>
                     </div>
 			</div>
-
-					<hr class="mt-5">
-	<?php if ($bill['status'] != PAYMENT_PAID): ?>
-					<p class="lead">Payment : <?php echo $state[$bill['status']]; ?></p>
-					<?php 
-						$cash = round((float) $bill['cash'], 2);
-						$card = round((float) $bill['card'], 2);
-						if ($card + $cash != 0) :
-							$total_short = round( (float) $bill['amount'], 2) - ($card + $cash);
-							?>
-								<p>
-									Short : <?php echo $total_short; ?> &euro; (card : <?php echo $card; ?> &euro;, cash : <?php echo $cash; ?> &euro;)
-								</p>
-						<?php endif; ?>
-						
-					<form action="<?php echo base_url(); ?>invoice/bill_pay/<?php echo $bill_id ?>" method="post" autocomplete="off">
-		<div class="input-group mb-3">
-			<div class="input-group-prepend">
-				<span class="input-group-text" for="exampleCheck1"><a href="#" id="select_card" onclick="event.preventDefault()"><i class="fab fa-cc-visa"></i>&nbsp;Card</a></span>
-			</div>
-			<input type="text" class="form-control" id="card_value" name="card_value" value="<?php echo ($card != 0) ? $card:'';?>" />
-			<div class="input-group-append">
-				<span class="input-group-text">&euro;</span>
-			</div>
-		</div>
-		
-		<div class="input-group mb-3">
-		  <div class="input-group-prepend">
-			<span class="input-group-text" for="exampleCheck1"><a href="#" id="select_cash" onclick="event.preventDefault()"><i class="fas fa-euro-sign"></i>&nbsp; Cash</a></span>
-		  </div>
-		  <input type="text" class="form-control" id="cash_value" name="cash_value" value="<?php echo ($cash != 0) ? $cash:'';?>" />
-		<div class="input-group-append">
-			<span class="input-group-text">&euro;</span>
-		</div>
-		</div>
-			<button type="submit" name="submit" value="1" class="btn btn-outline-success my-3 mx-3"><i class="fas fa-file-invoice-dollar"></i> Payment Complete</button>
-		
-		<?php endif; ?>
-		<?php if ($bill['status'] == PAYMENT_PAID): ?>
-		<p class="lead">Payment : <?php echo $state[$bill['status']]; ?>!</p>
-		<?php 
-			$cash = round((float) $bill['cash'], 2);
-			$card = round((float) $bill['card'], 2);
-		?>
-		Payed : <?php echo $bill['amount']; ?> &euro; (card : <?php echo $card; ?> &euro;, cash : <?php echo $cash; ?> &euro;)
-		<!-- <a href="<?php echo base_url(); ?>invoice/print_bill/<?php echo $bill_id; ?>" class="btn btn-outline-secondary my-3 mx-3" target="_blank"><i class="fas fa-print"></i> print</a> -->
-
-		<?php endif; ?>
-			<?php if ($bill['status'] != PAYMENT_PAID): ?>
-			<a href="<?php echo base_url(); ?>invoice/bill_unpay/<?php echo $bill_id; ?>" class="btn btn-outline-danger my-3 mx-3" target="_blank"><i class="fas fa-syringe"></i> Drop from stock</a>	
-		<?php endif; ?>
-		  </form>
 		</div>
 	</div>
 	</div>
@@ -194,6 +198,19 @@ document.addEventListener("DOMContentLoaded", function(){
 		$("#cash_selector").prop("checked", true);
 		$("#cash_value").val("<?php echo $bill['amount']; ?>");
 		$("#card_value").val("");
+	});
+	
+	$("#calculate").click(function() { 
+		var cash = parseFloat($("#cash_value").val());
+		var card = parseFloat($("#card_value").val());
+		var total = parseFloat(<?php echo $bill['amount']; ?>);
+		
+		if (isNaN(cash)) {cash = 0.0}  
+		if (isNaN(card)) {card = 0.0}  
+		if (isNaN(total)) {return false;}  
+		var total_in = cash+card;
+		
+		$("#payment_info").html("current: " + Math.round((total-total_in)*100)/100 + " &euro;");		
 	});
 });
 </script>
