fe2305b2759a478d44790cbe7d54f06a30cf764e
svenn <svenn.dhert@uantwerpen.be>
2021-01-03 11:12:26 +0100

    slight refactor on exports
    Signed-off-by: svenn <svenn.dhert@uantwerpen.be>

--- application/controllers/Export.php
+++ application/controllers/Export.php
@@ -38,61 +38,38 @@ class Export extends Admin_Controller
 		$domtree = new DOMDocument('1.0', 'Windows-1252');
 
 		/* create the root element of the xml tree */
-		$xmlRoot = $domtree->createElement("ImportExpMPlus");
+		$xmlRoot = $this->MPlus_headers($domtree);
 		
-		/* attributes */
-		$schema_xsi = $domtree->createAttribute('xmlns:xsi');
-		$schema_xsi->value = 'http://www.w3.org/2001/XMLSchema-instance';
-			
-		$schema_xsd = $domtree->createAttribute('xmlns:xsd');
-		$schema_xsd->value = 'http://www.w3.org/2001/XMLSchemae';
-		
-		$xmlRoot->appendChild($schema_xsi);
-		$xmlRoot->appendChild($schema_xsd);
-		
-		/* append it to the document created */
-		$xmlRoot = $domtree->appendChild($xmlRoot);
-
+		/* set template */
 		$Customers = $domtree->createElement("Customers");
 		$Customers = $xmlRoot->appendChild($Customers);
 		
-
 		foreach ($clients as $client) {
+			/* set item */
 			$cust = $domtree->createElement("Customer");
 			$cust = $Customers->appendChild($cust);
 			
-			$cust->appendChild($domtree->createElement('Prime', $client['id']));
-			// $cust->appendChild($domtree->createElement('Name', htmlspecialchars($client['first_name'] . ' ' . $client['last_name'])));
-			$cust->appendChild($domtree->createElement('Name', htmlspecialchars($client['last_name'])));
-			$cust->appendChild($domtree->createElement('Country', 'BE'));
-			$cust->appendChild($domtree->createElement('Street', $client['street']));
-			$cust->appendChild($domtree->createElement('HouseNumber', $client['nr']));
-			$cust->appendChild($domtree->createElement('ZipCode', $client['zip']));
-			$cust->appendChild($domtree->createElement('City', $client['city']));
-			$cust->appendChild($domtree->createElement('Language', $client['language']));
-			$cust->appendChild($domtree->createElement('CurrencyCode', 'EUR'));
-			$cust->appendChild($domtree->createElement('VATCode', '1'));
-			if ($client['btw_nr']) {
-				$cust->appendChild($domtree->createElement('VATStatus', '1'));
-			} else {
-				$cust->appendChild($domtree->createElement('VATStatus', '0'));
-			}
-			if ($client['btw_nr']) {
-				$cust->appendChild($domtree->createElement('VATNumber', $client['btw_nr']));
-			}
-			if ($client['telephone']) {
-				$cust->appendChild($domtree->createElement('Phone', $client['telephone']));
-			}
-			if ($client['mobile']) {
-				$cust->appendChild($domtree->createElement('GSM', $client['mobile']));
-			}
-			if ($client['mail']) {
-				$cust->appendChild($domtree->createElement('Email', $client['mail']));
-			}
-			if ($client['msg']) {
-				$cust->appendChild($domtree->createElement('FreeField1', $client['msg']));
-			}
-			$cust->appendChild($domtree->createElement('Status', '1'));
+			$this->append_child_element($cust, $domtree, 
+				array(
+							'Prime' 		=> $client['id'],
+							'Name' 			=> htmlspecialchars($client['last_name']),
+							'Country'		=> 'BE',
+							'Street' 		=> $client['street'],
+							'HouseNumber' 	=> $client['nr'],
+							'ZipCode' 		=> $client['zip'],
+							'City' 			=> $client['city'],
+							'Language' 		=> $client['language'],
+							'CurrencyCode' 	=> 'EUR',
+							'VATCode' 		=> '1',
+							'VATStatus'		=> (($client['btw_nr']) ? '1' : '0' ),
+							'VATNumber'		=> (($client['btw_nr']) ? $client['btw_nr'] : ''),
+							'Phone'			=> (($client['telephone']) ? $client['telephone'] : ''),
+							'GSM'			=> (($client['mobile']) ? $client['mobile'] : ''),
+							'Email'			=> (($client['mail']) ? $client['mail'] : ''),
+							'FreeField1'	=> (($client['msg']) ? $client['msg'] : ''),
+							'Status'		=> '1', // status : 0 : new, 1 : already known, 2 : known but editted 
+							
+				));
 		}
 		
 		/* get the xml printed */
@@ -110,28 +87,16 @@ class Export extends Admin_Controller
 		
 		if (!$bill_overview) {
 			echo "No valid result to be shown.";
-			exit;
+			return false;
 		}
 		
 		/* create a dom document with encoding utf8 */
 		$domtree = new DOMDocument('1.0', 'Windows-1252');
 
 		/* create the root element of the xml tree */
-		$xmlRoot = $domtree->createElement("ImportExpMPlus");
-		
-		/* attributes */
-		$schema_xsi = $domtree->createAttribute('xmlns:xsi');
-		$schema_xsi->value = 'http://www.w3.org/2001/XMLSchema-instance';
-			
-		$schema_xsd = $domtree->createAttribute('xmlns:xsd');
-		$schema_xsd->value = 'http://www.w3.org/2001/XMLSchemae';
-		
-		$xmlRoot->appendChild($schema_xsi);
-		$xmlRoot->appendChild($schema_xsd);
-		
-		/* append it to the document created */
-		$xmlRoot = $domtree->appendChild($xmlRoot);
+		$xmlRoot = $this->MPlus_headers($domtree);
 
+		/* set template */
 		$Sales = $domtree->createElement("Sales");
 		$Sales = $xmlRoot->appendChild($Sales);
 		
@@ -159,9 +124,9 @@ class Export extends Admin_Controller
 		
 			/* query events */
 			$events = $this->events
-			->where(array("payment" => $bill_id))
-			->fields('id, pet, payment')
-			->get_all();
+				->where(array("payment" => $bill_id))
+				->fields('id, pet, payment')
+				->get_all();
 			
 			/* if events fail don't even book this */
 			if (!$events) {
@@ -186,6 +151,7 @@ class Export extends Admin_Controller
 				}
 			}
 			
+			/* set item */
 			$Sale = $domtree->createElement("Sale");
 			$Sale = $Sales->appendChild($Sale);
 		
@@ -224,13 +190,17 @@ class Export extends Admin_Controller
 				$total_btw += round(($btw/100)*$tally, 2);
 			}
 			
-			$Sale->appendChild($domtree->createElement('Customer_Prime', $factuur['owner_id']));
-			$Sale->appendChild($domtree->createElement('DocType', 10));
-			$Sale->appendChild($domtree->createElement('DocNumber', date("Y") . substr(str_pad($factuur['id'], 5, "0", STR_PAD_LEFT), 0, 5)));
-			$Sale->appendChild($domtree->createElement('DocDate', $dt->format('d/m/Y')));
-			$Sale->appendChild($domtree->createElement('Amount', $this->amount($factuur['amount'])));
-			$Sale->appendChild($domtree->createElement('VATAmount', $this->amount($total_btw)));
-			$Sale->appendChild($domtree->createElement('Status', 0));
+			$this->append_child_element($Sale, $domtree, 
+				array(
+							'Customer_Prime'	=> $factuur['owner_id'],
+							'DocType' 			=> '10',
+							'DocNumber'			=> date("Y") . substr(str_pad($factuur['id'], 5, "0", STR_PAD_LEFT), 0, 5),
+							'DocDate' 			=> $dt->format('d/m/Y'),
+							'Amount' 			=> $this->amount($factuur['amount']),
+							'VATAmount' 		=> $this->amount($total_btw),
+							'Status' 			=> '0',
+							
+				));
 			
 			/*
 				a line for everything on the bill
@@ -253,18 +223,26 @@ class Export extends Admin_Controller
 			$detail = $domtree->createElement("Detail");
 			$detail = $Details->appendChild($detail);
 			
-			$detail->appendChild($domtree->createElement('Account', 400000)); // 400000 == booking of full amount
-			$detail->appendChild($domtree->createElement('Amount', $this->amount($factuur['amount'])));
-			$detail->appendChild($domtree->createElement('DebCre', 1));
-			$detail->appendChild($domtree->createElement('Ventil', 0)); // for full amount this must be 0
+			$this->append_child_element($detail, $domtree, 
+				array(
+							'Account'			=> 400000, // 400000 == booking of full amount
+							'Amount' 			=> $this->amount($factuur['amount']),
+							'DebCre'			=> 1,
+							'Ventil' 			=> 0, // for full amount this must be 0
+							
+				));
 				
 			$detail = $domtree->createElement("Detail");
 			$detail = $Details->appendChild($detail);
 			
-			$detail->appendChild($domtree->createElement('Account', 451000)); // 451000 == btw to pay
-			$detail->appendChild($domtree->createElement('Amount', $this->amount($total_btw)));
-			$detail->appendChild($domtree->createElement('DebCre', -1));
-			$detail->appendChild($domtree->createElement('Ventil', 11)); // btw
+			$this->append_child_element($detail, $domtree, 
+				array(
+							'Account'			=> 451000, // 451000 == btw to pay
+							'Amount' 			=> $this->amount($total_btw),
+							'DebCre'			=> -1,
+							'Ventil' 			=> 11, // btw
+							
+				));
 			
 			# booking codes
 			foreach ($event_booking	as $booking => $tally) {
@@ -272,22 +250,15 @@ class Export extends Admin_Controller
 
 				$detail = $domtree->createElement("Detail");
 				$detail = $Details->appendChild($detail);
-			
-				if ($current_booking_code['btw'] == 0) {
-					$current_btw = 1;
-				} elseif ($current_booking_code['btw'] == 6) {
-					$current_btw = 2;
-				} elseif ($current_booking_code['btw'] == 12) {
-					$current_btw = 3;
-				} elseif ($current_booking_code['btw'] == 21) {
-					$current_btw = 4;
-				} else {
-					$current_btw = -1;
-				}
-				$detail->appendChild($domtree->createElement('Account', $current_booking_code['code']));
-				$detail->appendChild($domtree->createElement('Amount', $this->amount($tally)));
-				$detail->appendChild($domtree->createElement('DebCre', -1));
-				$detail->appendChild($domtree->createElement('Ventil', $current_btw));
+				
+				$this->append_child_element($detail, $domtree, 
+					array(
+								'Account'			=> $current_booking_code['code'],
+								'Amount' 			=> $this->amount($tally),
+								'DebCre'			=> -1,
+								'Ventil' 			=> $this->get_btw_id($current_booking_code['btw']), // btw
+								
+					));
 			}
 		}
 		/* get the xml printed */
@@ -295,8 +266,68 @@ class Export extends Admin_Controller
 		echo $domtree->saveXML();
 	}
 	
+	/*
+		btw codes in Belgium
+	*/
+	private function get_btw_id($btw)
+	{
+		if ($btw == 0) {
+			return 1;
+		} elseif ($btw == 6) {
+			return 2;
+		} elseif ($btw == 12) {
+			return 3;
+		} elseif ($btw == 21) {
+			return 4;
+		}
+		
+		return -1;
+	}
+	
+	/*
+		loop through the data and put it in childs
+	*/
+	private function append_child_element($father, $domtree, $data)
+	{
+		foreach ($data as $key => $value)
+		{
+			// if no value skip it
+			if (empty($value)) { 
+				continue; 
+			}
+			$father->appendChild($domtree->createElement($key, $value));
+		}
+	}
+	
+	/*
+		change number formatting
+	*/
 	private function amount($value)
 	{
 		return number_format($value, 2, ',', '');
+	}
+	
+	/*
+		headers specific to Expert/M bookkeeping
+	*/
+	private function MPlus_headers($domtree)
+	{
+		/* create the root element of the xml tree */
+		$xmlRoot = $domtree->createElement("ImportExpMPlus");
+		
+		/* attributes */
+		$schema_xsi = $domtree->createAttribute('xmlns:xsi');
+		$schema_xsi->value = 'http://www.w3.org/2001/XMLSchema-instance';
+			
+		$schema_xsd = $domtree->createAttribute('xmlns:xsd');
+		$schema_xsd->value = 'http://www.w3.org/2001/XMLSchemae';
+		
+		$xmlRoot->appendChild($schema_xsi);
+		$xmlRoot->appendChild($schema_xsd);
+		
+		/* append it to the document created */
+		$xmlRoot = $domtree->appendChild($xmlRoot);
+		
+		return $xmlRoot;
 	}
 }
