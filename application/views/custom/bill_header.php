24f90e17bcab19bf76a309b65b83ba08ca283f44
svennd <svenn.dhert@uantwerpen.be>
2021-08-21 12:42:58 +0200

    - manually added dompdf 1.0.2

---
+++ application/views/custom/bill_header.php
@@ -0,0 +1,13 @@
+  <table width="100%">
+    <tr>
+        <td valign="top"><img src="<?php echo base_url(); ?>assets/custom/dommel.jpg" alt=""/></td>
+        <td align="right">
+			<pre>
+					Dr. M. Janssens
+					Dr. A. Janssens
+					Dr. C. Van Hoye
+					Dr. J. Van Puyvelde
+            </pre>
+        </td>
+    </tr>
+  </table>
