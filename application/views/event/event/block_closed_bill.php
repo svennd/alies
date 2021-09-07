7b7a3de10f0407c95e3f7a54e461e4ddd60fcf44
svennd <svenn.dhert@uantwerpen.be>
2021-09-07 22:31:12 +0200

    catch 0-case

--- application/views/event/event/block_closed_bill.php
+++ application/views/event/event/block_closed_bill.php
@@ -2,12 +2,16 @@
 	<div class="card-header">Bill</div>
 	<div class="card-body">
 			<ul>
-		<?php foreach($consumables as $prod) : ?>
-			<li><?php echo $prod['volume'] . ' ' . $prod['product']['unit_sell']  . ' ' . $prod['product']['name']; ?></li>
-		<?php endforeach; ?>
-		<?php foreach($procedures_d as $proc) : ?>
-			<li><?php echo $proc['amount'] . ' ' . $proc['procedures']['name']; ?></li>
-		<?php endforeach; ?>
+		<?php if($consumables): ?>
+			<?php foreach($consumables as $prod) : ?>
+				<li><?php echo $prod['volume'] . ' ' . $prod['product']['unit_sell']  . ' ' . $prod['product']['name']; ?></li>
+			<?php endforeach; ?>
+		<?php endif; ?>
+		<?php if($procedures_d): ?>
+			<?php foreach($procedures_d as $proc) : ?>
+				<li><?php echo $proc['amount'] . ' ' . $proc['procedures']['name']; ?></li>
+			<?php endforeach; ?>
+		<?php endif; ?>
 		</ul>
 		<a href="<?php echo base_url(); ?>invoice/get_bill/<?php echo $event_info['payment']; ?>" class="btn btn-outline-success"><i class="fas fa-arrow-right"></i> Show bill</a>
 	</div>
