f553d68571325ad0b3ad8dd50e5d4705751f6c47
svennd <svenn.dhert@uantwerpen.be>
2022-05-06 13:26:39 +0200

    complex names

--- application/views/reports/stock_list.php
+++ application/views/reports/stock_list.php
@@ -1,5 +1,5 @@
 name,volume,unit,lotnr,eol,volumes,created_at
 <?php foreach($stock_list as $stock): ?>
-<?php echo $stock['name']; ?>, <?php echo $stock['volume']; ?>, <?php echo $stock['unit_buy']; ?>, <?php echo $stock['lotnr']; ?>, <?php echo $stock['eol']; ?>, <?php echo $stock['concat_volume']; ?>, <?php echo $stock['created_at']; ?>
+"<?php echo $stock['name']; ?>", <?php echo $stock['volume']; ?>, <?php echo $stock['unit_buy']; ?>, <?php echo $stock['lotnr']; ?>, <?php echo $stock['eol']; ?>, <?php echo $stock['concat_volume']; ?>, <?php echo $stock['created_at']; ?>
 
 <?php endforeach; ?>
