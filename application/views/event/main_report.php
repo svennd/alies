a1a1b84d657cddfafa79e0c1a7f6b7147c95e747
svennd <svenn.dhert@uantwerpen.be>
2022-07-22 10:31:00 +0200

    no use in extra link

--- application/views/event/main_report.php
+++ application/views/event/main_report.php
@@ -55,8 +55,7 @@
 		<div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
 			Report
 			<div class="dropdown no-arrow">
-				<a href="<?php echo base_url(); ?>invoice/get_bill/<?php echo $event_info['payment']; ?>" class="btn btn-outline-success btn-sm"><i class="fas fa-arrow-right"></i> Show Invoice</a>
-				<a href="<?php echo base_url(); ?>invoice/get_bill/<?php echo $event_info['payment']; ?>" class="btn btn-outline-success btn-sm"><i class="fas fa-print"></i> Print Invoice</a>
+				<a href="<?php echo base_url(); ?>invoice/get_bill/<?php echo $event_info['payment']; ?>/print" class="btn btn-outline-success btn-sm"><i class="fas fa-print"></i> Print Invoice</a>
 				<?php if($event_info['no_history'] == 1): ?>
 				<a href="<?php echo base_url(); ?>events/enable_history/<?php echo $event_id; ?>" role="button" id="dropdownMenuLink" class="btn btn-outline-primary btn-sm">
 				<i class="fas fa-eye"></i> Enable Report
