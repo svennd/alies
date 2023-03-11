108e5077851b39e8b5b37495a5cb795f47335312
svennd <svenn.dhert@uantwerpen.be>
2023-03-11 18:21:51 +0100

    increase bill generation to 250 year

--- application/helpers/generate_bill_id_helper.php
+++ application/helpers/generate_bill_id_helper.php
@@ -1,7 +1,8 @@
 <?php
 
+// at the rate of 4k numbers / year we can generate for ~250 year before turnover
 function get_bill_id(int $id, $input_date = false)
 {
     $date = ($input_date) ? DateTime::createFromFormat('Y-m-d H:i:s', $input_date) : new DateTime();
-    return $date->format("Y") . str_pad($id, 5, '0', STR_PAD_LEFT);
+    return $date->format("Y") . str_pad($id, 6, '0', STR_PAD_LEFT);
 }
