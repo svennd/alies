f8bb2fb48c8f20cbf6e1628187736436c18751c8
svennd <svenn.dhert@uantwerpen.be>
2023-06-28 18:52:12 +0200

    allow locking for vets

--- application/views/event/event/block_event_controller.php
+++ application/views/event/event/block_event_controller.php
@@ -8,10 +8,8 @@
 						<?php if (!$consumables && !$procedures_d && empty($event_info['anamnese']) && empty($event_info['title']) && !$event_uploads): ?>
 							<a href="<?php echo base_url(); ?>events/del/<?php echo $event_id; ?>/<?php echo $owner['id']; ?>" class="btn btn-danger btn-sm"><i class="fas fa-trash-alt"></i> Remove event</a>
 						<?php else : ?>
-							<?php if ($this->ion_auth->in_group("admin")): ?>
 							<a href="<?php echo base_url(); ?>events/lock/<?php echo $event_id; ?>" class="btn btn-warning btn-sm"><i class="fas fa-lock"></i> Lock event</a>
 							<br/>
-							<?php endif; ?>
 							<a href="<?php echo base_url(); ?>events/edit_price/<?php echo $event_id; ?>" class="btn btn-danger btn-sm mt-3"><i class="fas fa-skull-crossbones"></i> Edit Price</a>
 						<?php endif; ?>
 					<?php elseif($event_info['status'] == STATUS_CLOSED): ?>
