dfbb5886e7789b1547751749fb12c66222f76e50
svennd <svenn.dhert@uantwerpen.be>
2023-10-31 15:39:06 +0100

    improve debugging tool

--- application/views/logs/product.php
+++ application/views/logs/product.php
@@ -3,7 +3,7 @@
 
       <div class="card shadow mb-4">
 			<div class="card-header">
-				<a href="<?php echo base_url(); ?>logs">Logs</a> / detail transaction product log
+				<a href="<?php echo base_url(); ?>logs">Logs</a> / <a href="<?php echo base_url('products/profile/' . $logs['0']['product']['id']); ?>"><?php echo $logs['0']['product']['name']; ?></a> / detail transaction log
 			</div>
             <div class="card-body">
 			<?php if ($logs): ?>
@@ -12,7 +12,6 @@
 				<thead>
 				<tr>
 					<th>Event</th>
-					<th>product</th>
 					<th>Volume</th>
 					<th>Vet</th>
 					<th>Location</th>
@@ -23,8 +22,7 @@
 				<?php foreach ($logs as $log): ?>
 				<tr>
 					<td><?php echo $log['event']; ?></td>
-					<td><?php echo $log['product']['name']; ?></td>
-					<td><?php echo $log['volume']; ?> <?php echo $log['product']['unit_sell']; ?></td>
+					<td><?php echo ($log['volume'] >= 0) ? '<p style="color:green">'  . $log['volume'] .  ' ' . $log['product']['unit_sell'] . '</p>' : '<p style="color:red">' . $log['volume'] .  ' ' . $log['product']['unit_sell'] . '</p>'; ?></td>
 					<td><?php echo $log['vet']['first_name']; ?></td>
 					<td><?php echo $log['locations']['name']; ?></td>
 					<td data-sort="<?php echo strtotime($log['created_at']); ?>"><?php echo $log['created_at']; ?></td>
@@ -47,6 +45,6 @@ document.addEventListener("DOMContentLoaded", function(){
 	$("#adminmgm").show();
 	$("#admin").addClass('active');
 	$("#logs").addClass('active');
-	$("#dataTable").DataTable({"order": [[ 5, "desc" ]]});
+	$("#dataTable").DataTable({"order": [[ 4, "desc" ]]});
 });
 </script>
