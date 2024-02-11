55427f34f113c3081f9426188659c4396ea59a94
svennd <svenn.dhert@uantwerpen.be>
2024-02-11 11:54:31 +0100

    modify path to trumbowyg injector

--- application/helpers/inject_helper.php
+++ application/helpers/inject_helper.php
@@ -9,14 +9,12 @@ function inject_trumbowyg(string $field = "footer")
 {
   return ($field == "header") ?
     '
-    <link href="'. base_url() .'assets/css/trumbowyg.min.css" rel="stylesheet">
+    <link href="'. base_url('node_modules/trumbowyg/dist/ui/trumbowyg.min.css').'" rel="stylesheet">
     '
   :
     '
-      <script src="'. base_url() .'assets/js/trumbowyg.min.js"></script>
-      <script src="'. base_url() .'assets/js/plugins/cleanpaste/trumbowyg.cleanpaste.min.js"></script>
-      <script src="'. base_url() .'assets/js/plugins/fontsize/trumbowyg.fontsize.min.js"></script>
-      <script src="'. base_url() .'assets/js/plugins/template/trumbowyg.template.min.js"></script>
+      <script src="'. base_url('node_modules/trumbowyg/dist/trumbowyg.min.js') .'"></script>
+      <script src="'. base_url('node_modules/trumbowyg/dist/plugins/template/trumbowyg.template.min.js') .'"></script>
     '
   ;
 }
