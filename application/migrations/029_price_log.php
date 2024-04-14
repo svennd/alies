6091b75f836cb3c11a8a61e3eb883d53663200b9
svennd <svenn.dhert@uantwerpen.be>
2024-04-14 15:40:46 +0200

    allow null in register in updated

--- application/migrations/029_price_log.php
+++ application/migrations/029_price_log.php
@@ -22,6 +22,11 @@ class Migration_price_log extends CI_Migration {
 		# increase size for storing values
 		# it was 5,2 which is way too small
 		$sql[] = "ALTER TABLE `lab_detail` CHANGE `value` `value` DECIMAL(10,2) NOT NULL, CHANGE `upper_limit` `upper_limit` DECIMAL(10,2) NOT NULL, CHANGE `lower_limit` `lower_limit` DECIMAL(10,2) NOT NULL;";
+		
+		# set updated_at to null
+		# since it can never be updated
+		$sql[] = "ALTER TABLE `register_in` CHANGE `updated_at` `updated_at` DATETIME NULL DEFAULT NULL;";
+		$sql[] = "update register_in set updated_at = null;";
 
 		foreach ($sql as $q)
 		{
