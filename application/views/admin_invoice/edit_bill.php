e91f971e3f46f80f775a1d7008c771d1f553cbf0
svennd <svenn.dhert@uantwerpen.be>
2024-05-05 11:42:23 +0200

    remove rm_bill, manually do it!

--- application/views/admin_invoice/edit_bill.php
+++ application/views/admin_invoice/edit_bill.php
@@ -149,30 +149,11 @@
             </div>
             </div>
         </div>
-
-
         <button type="submit" name="submit" value="1" class="btn btn-primary">Edit</button>
     </form>
     </div>
 
     
-    <div class="col-lg-6">
-            <div class="card border-danger my-3" style="height:100%;">
-                <div class="card-header <?php echo (!is_null($bill['invoice_id'])) ? "text-secondary" : "text-danger"; ?>">Remove bill</div>
-
-                <div class="card-body">
-                    <form action="<?php echo base_url('admin_invoice/rm_bill/' . $bill['id']); ?>" method="post" autocomplete="off">
-                        <div class="form-row mb-3">
-                            <div class="col text-danger">
-                                <label for="exampleFormControlInput3">delete reason</label>
-                                <input type="text" class="form-control" name="reason">
-                            </div>
-                        </div>
-                        <button type="submit" name="submit" value="1" class="btn btn-danger" <?php echo (!is_null($bill['invoice_id'])) ? "disabled" : ""; ?>>Delete bill</button>
-                    </form>
-                </div>
-               </div>
-    </div>
 </div>
 
 <script type="text/javascript">
