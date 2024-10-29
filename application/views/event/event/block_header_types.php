32442cb8a3d98ef9dc07fe087d98b17dea33b4c6
svennd <svenn.dhert@uantwerpen.be>
2024-10-29 18:49:29 +0100

    smash them togheter :)

--- application/views/event/event/block_header_types.php
+++ application/views/event/event/block_header_types.php
@@ -4,14 +4,17 @@
         <i class="fas fa-eye"></i><span>&nbsp;<?php echo $this->lang->line('enable_report'); ?></span>
     </a>
     <?php else: ?>
-    <a href="<?php echo base_url('events_report/set_type/' . $event_id . '/' . DISEASE); ?>" role="button" class="btn btn-outline-primary btn-sm <?php echo ($event_info["type"] == DISEASE) ? "btn-outline-success" : "" ?>">
-        <i class="fas fa-virus"></i><span>&nbsp;<?php echo $this->lang->line('disease'); ?></span>
-    </a>
-    <a href="<?php echo base_url('events_report/set_type/'. $event_id . '/' . OPERATION); ?>" role="button" class="btn btn-outline-primary btn-sm <?php echo ($event_info["type"] == OPERATION) ? "btn-outline-success" : "" ?>">
-        <i class="fas fa-hand-holding-medical"></i><span>&nbsp;<?php echo $this->lang->line('operation'); ?></span>
-    </a>
+    <div class="btn-group" role="group">
+        <a href="<?php echo base_url('events_report/set_type/' . $event_id . '/' . DISEASE); ?>" role="button" class="btn btn-outline-primary btn-sm <?php echo ($event_info["type"] == DISEASE) ? "btn-outline-success" : "" ?>">
+            <i class="fas fa-virus"></i><span>&nbsp;<?php echo $this->lang->line('disease'); ?></span>
+        </a>
+        <a href="<?php echo base_url('events_report/set_type/'. $event_id . '/' . OPERATION); ?>" role="button" class="btn btn-outline-primary btn-sm <?php echo ($event_info["type"] == OPERATION) ? "btn-outline-success" : "" ?>">
+            <i class="fas fa-hand-holding-medical"></i><span>&nbsp;<?php echo $this->lang->line('operation'); ?></span>
+        </a>
+    </div>
+    
     <a href="<?php echo base_url('events_report/disable_history/' . $event_id); ?>" role="button" id="dropdownMenuLink" class="btn btn-outline-danger btn-sm">
         <i class="fas fa-eye-slash"></i><span>&nbsp;<?php echo $this->lang->line('disable_report'); ?></span>
     </a>
     <?php endif; ?>
-</div>	
+</div>
