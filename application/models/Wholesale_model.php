865d47144034182a85950cd7cbab6c408860a495
svennd <svenn.dhert@uantwerpen.be>
2025-02-03 20:06:30 +0100

    since was wrong

--- application/models/Wholesale_model.php
+++ application/models/Wholesale_model.php
@@ -45,7 +45,7 @@ class Wholesale_model extends MY_Model
 					last_bruto,
 					wholesale.id as wh_id,
 					ROUND(((bruto - last_bruto) / last_bruto) * 100, 1) AS percentage_change,
-					wholesale.updated_at as up
+					wholesale.last_bruto_date as up
 				FROM
 					wholesale
 				JOIN
